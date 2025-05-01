d97d8f3e9a9be2bb3f785147ebd7b4447da04ad7
Eugene <eugene@amberpixels.io>
2025-05-01 17:19:58 +0300

    abureflect

---
+++ reflect/reflect.go
@@ -0,0 +1,25 @@
+// Package abureflect contains helpers that extends standard reflect library
+package abureflect
+
+import "reflect"
+
+// IndirectDeep does reflect.Indirect deeply.
+func IndirectDeep(v reflect.Value) reflect.Value {
+	for v.Kind() == reflect.Pointer {
+		v = v.Elem()
+	}
+	return v
+}
+
+// LengthOf returns length of a given type.
+func LengthOf(a any) (int, bool) {
+	if a == nil {
+		return 0, false
+	}
+	switch reflect.TypeOf(a).Kind() {
+	case reflect.Map, reflect.Array, reflect.String, reflect.Chan, reflect.Slice:
+		return reflect.ValueOf(a).Len(), true
+	default:
+		return 0, false
+	}
+}
